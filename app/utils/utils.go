20d415b2dd0d0753d9b74f5a892af43585d411d6
root <root@localhost.localdomain>
2019-03-07 01:24:34 +0000

    19-03-07-2th

--- app/utils/utils.go
+++ app/utils/utils.go
@@ -103,7 +103,7 @@ func ParseTemplateFiles(filenames ...string) (t *template.Template) {
 	var files []string
 	t = template.New("layout")
 	for _, file := range filenames {
-		files = append(files, fmt.Sprintf("templates/%s.html", file))
+		files = append(files, fmt.Sprintf("app/views/%s.html", file))
 	}
 	t = template.Must(t.ParseFiles(files...))
 	return
@@ -112,7 +112,7 @@ func ParseTemplateFiles(filenames ...string) (t *template.Template) {
 func GenerateHTML(writer http.ResponseWriter, data interface{}, filenames ...string) {
 	var files []string
 	for _, file := range filenames {
-		files = append(files, fmt.Sprintf("templates/%s.html", file))
+		files = append(files, fmt.Sprintf("app/views/%s.html", file))
 	}
 
 	templates := template.Must(template.ParseFiles(files...))
